5ac684d09714294d73109df013fdf366a721ee2c
Bo Li <bli0603@gmail.com>
2021-03-29 14:05:30 +1300

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Member } from '../_models/member';
-import { of } from 'rxjs';
+import { of, pipe } from 'rxjs';
 import { map, take } from 'rxjs/operators';
 import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
 import { User } from '../_models/user';
-
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +18,13 @@ export class MembersService {
   memberCache = new Map();
   user: User;
   userParams: UserParams;
-  
+
   constructor(private http: HttpClient, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=> {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
       this.user = user;
       this.userParams = new UserParams(user);
     })
-   }
+  }
 
   getUserParams() {
     return this.userParams;
@@ -52,8 +51,8 @@ export class MembersService {
     params = params.append('maxAge', userParams.maxAge.toString());
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
-    
-     return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
       .pipe(map(response => {
         this.memberCache.set(Object.values(userParams).join('-'), response);
         return response;
@@ -77,7 +76,7 @@ export class MembersService {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
-    );
+    )
   }
 
   setMainPhoto(photoId: number) {
@@ -86,6 +85,16 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  getLikes(predicate: string, pageNumber, pageSize) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('predicate', predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResult<T>(url, params) {
@@ -103,9 +112,10 @@ export class MembersService {
 
   private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
+
     params = params.append('pageNumber', pageNumber.toString());
     params = params.append('pageSize', pageSize.toString());
-  
+
     return params;
   }
 }
